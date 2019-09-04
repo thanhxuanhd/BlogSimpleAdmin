588d7c90a26698ee818b4ef46230aa86dff96c14
Nguyễn Thanh Xuân <thanhxuanhd007@gmail.com>
2019-09-05 00:14:32 +0700

    Update package and fix bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true }), NgbModule.forRoot()],
+    imports: [RouterModule.forRoot(routes, { useHash: true }), NgbModule],
     exports: [RouterModule],
     providers: [AuthGuardService]
 })
